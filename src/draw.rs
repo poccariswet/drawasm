1b41243fa4c364fc7ef53caf5b466e2d1c5c48d6
poccariswet <s1240231@gmail.com>
2019-12-18 20:50:11 +0900

    [WIP] add undo list and state method

--- src/draw.rs
+++ src/draw.rs
@@ -1,9 +1,12 @@
 use std::cell::Cell;
+use std::cell::RefCell;
 use std::f64;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, MouseEvent};
+
+use crate::state::State;
 
 // sample draw canvas
 //#[warn(unused_variables)]
@@ -46,7 +49,23 @@ use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, MouseEvent};
 //    context.stroke();
 //}
 
-pub fn canvas_draw_start(canvas: &HtmlCanvasElement) -> Result<(), JsValue> {
+/*
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(msg: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+*/
+
+// setup mouse event listener for drawing and start
+pub fn canvas_draw_start(
+    canvas: &HtmlCanvasElement,
+    state: &Rc<RefCell<State>>,
+) -> Result<(), JsValue> {
     let context = canvas
         .get_context("2d")
         .expect("Could not get context")
@@ -55,30 +74,60 @@ pub fn canvas_draw_start(canvas: &HtmlCanvasElement) -> Result<(), JsValue> {
         .unwrap();
     let pressed = Rc::new(Cell::new(false));
 
+    // mousedown
     {
         let context_copy = context.clone();
         let canvas_copy = canvas.clone();
+        let state_copy = state.clone();
         let pressed = pressed.clone();
+
         let mouse_down = Closure::wrap(Box::new(move |event: MouseEvent| {
+            let undo = state_copy.borrow_mut().get_undo();
+            match undo {
+                Some(u) => {
+                    context_copy.put_image_data(&u, 0.0, 0.0);
+                }
+                None => {}
+            }
+            pressed.set(true);
+            let image_data = context_copy
+                .get_image_data(
+                    0.0,
+                    0.0,
+                    state_copy.borrow().get_width() as f64,
+                    state_copy.borrow().get_height() as f64,
+                )
+                .unwrap();
+            console_log!("{:?}", image_data);
+            state_copy.borrow_mut().add_undo(image_data);
+            //console_log!("{}", canvas_copy.to_data_url().unwrap());
+
             let new_x = event.offset_x() as f64;
             let new_y = event.offset_y() as f64;
             context_copy.begin_path();
-            context_copy.set_stroke_style(&JsValue::from("#000000"));
-            context_copy.set_line_width(1.0);
+            context_copy.set_stroke_style(&JsValue::from(state_copy.borrow().get_color()));
+            context_copy.set_line_width(state_copy.borrow().get_pen_thin());
             context_copy.move_to(new_x, new_y);
-            pressed.set(true);
         }) as Box<dyn FnMut(_)>);
 
         canvas
             .add_event_listener_with_callback("mousedown", mouse_down.as_ref().unchecked_ref())?;
 
-        mouse_down.forget();
+        mouse_down.forget(); // memory leak
     }
 
+    // mouseup
     {
+        let context_copy = context.clone();
         let pressed = pressed.clone();
+
         let mouse_up = Closure::wrap(Box::new(move |event: MouseEvent| {
             pressed.set(false);
+            let new_x = event.offset_x() as f64;
+            let new_y = event.offset_y() as f64;
+            context_copy.fill_rect(new_x, new_y, 1.0, 1.0);
+            context_copy.line_to(new_x, new_y);
+            context_copy.stroke();
         }) as Box<dyn FnMut(_)>);
 
         canvas.add_event_listener_with_callback("mouseup", mouse_up.as_ref().unchecked_ref())?;
@@ -86,9 +135,11 @@ pub fn canvas_draw_start(canvas: &HtmlCanvasElement) -> Result<(), JsValue> {
         mouse_up.forget();
     }
 
+    // mousemove
     {
         let context_copy = context.clone();
         let pressed = pressed.clone();
+
         let mouse_move = Closure::wrap(Box::new(move |event: MouseEvent| {
             if pressed.get() {
                 let new_x = event.offset_x() as f64;
--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,43 @@
 pub struct State {
+    width: u32,
+    height: u32,
     pen_thin: f64,
     color: String,
+    undo_image_data: Vec<web_sys::ImageData>,
 }
 
 impl State {
-    pub fn new() -> State {
+    pub fn new(w: u32, h: u32) -> State {
         State {
+            width: w,
+            height: h,
             pen_thin: 1.0,                //TODO not hardcode
             color: "#000000".to_string(), //TODO not hardcode
+            undo_image_data: vec![],
         }
+    }
+
+    pub fn get_color(&self) -> String {
+        self.color.clone() // not implement Copy trait
+    }
+
+    pub fn get_pen_thin(&self) -> f64 {
+        self.pen_thin
+    }
+
+    pub fn get_width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn get_height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn add_undo(&mut self, data: web_sys::ImageData) {
+        self.undo_image_data.push(data);
+    }
+
+    pub fn get_undo(&mut self) -> Option<web_sys::ImageData> {
+        self.undo_image_data.pop()
     }
 }
