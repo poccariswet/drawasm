57853ccb1128fc3f6f42fdac17d1ebd13e6012b3
poccariswet <s1240231@gmail.com>
2019-12-18 20:53:34 +0900

    [FIX] delete commentout

--- src/draw.rs
+++ src/draw.rs
@@ -8,47 +8,6 @@ use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, MouseEvent};
 
 use crate::state::State;
 
-// sample draw canvas
-//#[warn(unused_variables)]
-//fn draw_face() {
-//    let document = web_sys::window().unwrap().document().unwrap();
-//    let canvas = document.get_element_by_id("canvas").unwrap();
-//    let canvas: web_sys::HtmlCanvasElement = canvas
-//        .dyn_into::<web_sys::HtmlCanvasElement>()
-//        .map_err(|_| ())
-//        .unwrap();
-//
-//    let context = canvas
-//        .get_context("2d")
-//        .unwrap()
-//        .unwrap()
-//        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-//        .unwrap();
-//
-//    context.begin_path();
-//
-//    // Draw the face
-//    context
-//        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-//        .unwrap();
-//
-//    // Draw the mouth.
-//    context.move_to(110.0, 75.0);
-//    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-//
-//    // Draw the left eye.
-//    context.move_to(65.0, 65.0);
-//    context.arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI).unwrap();
-//
-//    // Draw the right eye.
-//    context.move_to(95.0, 65.0);
-//    context
-//        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-//        .unwrap();
-//
-//    context.stroke();
-//}
-
 /*
 #[wasm_bindgen]
 extern "C" {
